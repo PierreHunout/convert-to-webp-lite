751c29c9efa1ec29401f7f1e682e1afdf22fbc7d
PierreHunout <pierre.hunout@gmail.com>
2025-07-24 01:56:39 +0100

    Add Deactivate hook

---
+++ includes/Actions/Deactivate.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ *
+ * @package WpConvertToWebp\Actions
+ * @since 1.0.0
+ */
+
+namespace WpConvertToWebp\Actions;
+
+use WpConvertToWebp\Cleaner;
+
+/**
+ * This check prevents direct access to the plugin file,
+ * ensuring that it can only be accessed through WordPress.
+ * 
+ * @since 1.0.0
+ */
+if (!defined('WPINC')) {
+    die;
+}
+
+class Deactivate
+{
+
+    public function run()
+    {
+        return;
+    }
+
+    /**
+     * Deactivate the plugin.
+     *
+     * This method is called when the plugin is deactivated.
+     * It deletes WebP files if the option is set.
+     *
+     * @since 1.0.0
+     */
+    public static function deactivate()
+    {
+        $delete_webp    = get_option('delete_webp_on_deactivate', false);
+
+        if ($delete_webp) {
+            try {
+                $files  = \WpConvertToWebp\Tools::get_files();
+
+                foreach ($files as $file) {
+                    if ($file->isFile() && strtolower($file->getExtension()) === 'webp') {
+						if (!@unlink($file->getPathname())) {
+							error_log('[WP Convert to WebP] Failed to delete: ' . $file->getPathname());
+						}
+
+						@unlink($file->getPathname());
+					}
+                }
+            } catch (\Throwable $e) {
+                error_log('[WP Convert to WebP] Deactivate error: ' . $e->getMessage());
+            }
+
+            delete_option('delete_webp_on_deactivate');
+            delete_option('convert_to_webp_quality');
+        }
+    }
+}

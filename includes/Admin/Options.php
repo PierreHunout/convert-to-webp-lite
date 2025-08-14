e6ea248565be03152018a8eef27c5e967c3c9917
PierreHunout <pierre.hunout@gmail.com>
2025-08-14 16:05:32 +0100

    FIX: HTML/CSS with BEM structure

--- includes/Admin/Options.php
+++ includes/Admin/Options.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is responsible for managing the options page of the WebP conversion plugin.
  * It includes methods to add the options page, render the form, handle form submissions,
@@ -11,7 +12,8 @@
 namespace WpConvertToWebp\Admin;
 
 use WpConvertToWebp\Tools;
-use Throwable;
+use WpConvertToWebp\Converter;
+use WpConvertToWebp\Cleaner;
 
 /**
  * This check prevents direct access to the plugin file,
@@ -79,54 +81,54 @@ class Options
         $delete_on_deactivate   = get_option('delete_webp_on_deactivate', false);
         $delete_on_uninstall    = get_option('delete_webp_on_uninstall', false);
 ?>
-        <div class="wrap convert-to-webp">
-            <h1><?php _e('WebP Conversion Options', 'wp-convert-to-webp'); ?></h1>
-            <div class="grid">
+        <div id="convert-to-webp" class="wrap convert-to-webp">
+            <h1 class="convert-to-webp__title"><?php _e('WebP Conversion Options', 'wp-convert-to-webp'); ?></h1>
+            <div class="convert-to-webp__grid">
                 <div class="convert-to-webp__forms">
                     <!-- Options form -->
                     <form method="post" action="" class="convert-to-webp__form convert-to-webp__form--options">
                         <?php wp_nonce_field('convert_to_webp_save_options'); ?>
-                        <div class="convert-to-webp__form--table">
-                            <div class="convert-to-webp__form--table__row">
-                                <h2><?php _e('WebP Quality', 'wp-convert-to-webp'); ?></h2>
-                                <div class="convert-to-webp__form--table__row__input">
-                                    <input type="number" id="convert_to_webp_quality" name="convert_to_webp_quality" min="0" max="100" value="<?php echo esc_attr($webp_quality); ?>">
-                                    <input type="range" id="convert_to_webp_quality_slider" min="0" max="100" value="<?php echo esc_attr($webp_quality); ?>" oninput="document.getElementById('convert_to_webp_quality').value = this.value;">
+                        <div class="convert-to-webp__table">
+                            <div class="convert-to-webp__row">
+                                <h2 class="convert-to-webp__subtitle"><?php _e('WebP Quality', 'wp-convert-to-webp'); ?></h2>
+                                <div class="convert-to-webp__inputs">
+                                    <input type="number" id="convert_to_webp_quality" class="convert-to-webp__input convert-to-webp__input--number" name="convert_to_webp_quality" min="0" max="100" value="<?php echo esc_attr($webp_quality); ?>">
+                                    <input type="range" id="convert_to_webp_quality_slider" class="convert-to-webp__input convert-to-webp__input--range" min="0" max="100" value="<?php echo esc_attr($webp_quality); ?>" oninput="document.getElementById('convert_to_webp_quality').value = this.value;">
                                 </div>
-                                <p class="description"><?php _e('Default: 85. Higher means better quality but larger files.', 'wp-convert-to-webp'); ?></p>
+                                <p class="convert-to-webp__description"><?php _e('Default: 85. Higher means better quality but larger files.', 'wp-convert-to-webp'); ?></p>
                             </div>
 
-                            <div class="convert-to-webp__form--table__row">
-                                <h2><?php _e('Clean data on <strong>deactivate</strong>', 'wp-convert-to-webp'); ?></h2>
-                                <div class="convert-to-webp__form--table__row__input">
-                                    <input type="checkbox" name="delete_webp_on_deactivate" value="1" <?php checked($delete_on_deactivate, 1); ?> />
-                                    <?php _e('Delete all WebP files', 'wp-convert-to-webp'); ?>
+                            <div class="convert-to-webp__row">
+                                <h2  class="convert-to-webp__subtitle"><?php _e('Clean data on <strong>deactivate</strong>', 'wp-convert-to-webp'); ?></h2>
+                                <div class="convert-to-webp__inputs">
+                                    <input type="checkbox" class="convert-to-webp__input convert-to-webp__input--toggle" name="delete_webp_on_deactivate" value="1" <?php checked($delete_on_deactivate, 1); ?> />
+                                    <label class="convert-to-webp__label"><?php _e('Delete all WebP files', 'wp-convert-to-webp'); ?></label>
                                 </div>
                             </div>
 
-                            <div class="convert-to-webp__form--table__row">
-                                <h2><?php _e('Clean data on <strong>uninstall</strong>', 'wp-convert-to-webp'); ?></h2>
-                                <div class="convert-to-webp__form--table__row__input">
-                                    <input type="checkbox" name="delete_webp_on_uninstall" value="1" <?php checked($delete_on_uninstall, 1); ?> />
-                                    <?php _e('Delete all WebP files', 'wp-convert-to-webp'); ?>
+                            <div class="convert-to-webp__row">
+                                <h2  class="convert-to-webp__subtitle"><?php _e('Clean data on <strong>uninstall</strong>', 'wp-convert-to-webp'); ?></h2>
+                                <div class="convert-to-webp__inputs">
+                                    <input type="checkbox"  class="convert-to-webp__input convert-to-webp__input--toggle" name="delete_webp_on_uninstall" value="1" <?php checked($delete_on_uninstall, 1); ?> />
+                                    <label class="convert-to-webp__label"><?php _e('Delete all WebP files', 'wp-convert-to-webp'); ?></label>
                                 </div>
                             </div>
                         </div>
-                        <div class="convert-to-webp__form--submit">
+                        <div class="convert-to-webp__submit">
                             <input type="hidden" name="action" value="save_options">
-                            <button type="submit" class="button button-primary"><?php _e('Save options', 'wp-convert-to-webp'); ?></button>
+                            <button type="submit" class="button button-primary convert-to-webp__button convert-to-webp__button--primary"><?php _e('Save options', 'wp-convert-to-webp'); ?></button>
                         </div>
                     </form>
 
                     <!-- Legacy conversion form -->
                     <form method="post" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" class="convert-to-webp__form convert-to-webp__form--legacy">
                         <?php wp_nonce_field('convert_webp_legacy'); ?>
-                        <div class="convert-to-webp__form--table">
-                            <div class="convert-to-webp__form--table__row">
-                                <h2><?php _e('Convert old images', 'wp-convert-to-webp'); ?></h2>
-                                <div class="convert-to-webp__form--submit convert-to-webp__form--submit__secondary">
+                        <div class="convert-to-webp__table">
+                            <div class="convert-to-webp__row">
+                                <h2 class="convert-to-webp__subtitle"><?php _e('Convert old images', 'wp-convert-to-webp'); ?></h2>
+                                <div class="convert-to-webp__submit convert-to-webp__submit--secondary">
                                     <input type="hidden" name="action" value="convert_to_webp_legacy">
-                                    <button type="submit" class="button"><?php _e('Convert all previously uploaded images', 'wp-convert-to-webp'); ?></button>
+                                    <button type="submit" class="button convert-to-webp__button"><?php _e('Convert all previously uploaded images', 'wp-convert-to-webp'); ?></button>
                                 </div>
                             </div>
                         </div>
@@ -135,12 +137,12 @@ class Options
                     <!-- Delete all WebP files form -->
                     <form method="post" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" class="convert-to-webp__form convert-to-webp__form--delete" onsubmit="return confirm('<?php echo esc_js(__('Are you sure you want to delete all WebP files?', 'wp-convert-to-webp')); ?>');">
                         <?php wp_nonce_field('delete_all_webp'); ?>
-                        <div class="convert-to-webp__form--table">
-                            <div class="convert-to-webp__form--table__row">
-                                <h2><?php _e('Delete all WebP files', 'wp-convert-to-webp'); ?></h2>
-                                <div class="convert-to-webp__form--submit convert-to-webp__form--submit__secondary">
+                        <div class="convert-to-webp__table">
+                            <div class="convert-to-webp__row">
+                                <h2 class="convert-to-webp__subtitle"><?php _e('Delete all WebP files', 'wp-convert-to-webp'); ?></h2>
+                                <div class="convert-to-webp__submit convert-to-webp__submit--secondary">
                                     <input type="hidden" name="action" value="delete_all_webp">
-                                    <button type="submit" class="button button-danger"><?php _e('Delete all WebP files', 'wp-convert-to-webp'); ?></button>
+                                    <button type="submit" class="button button-danger convert-to-webp__button convert-to-webp__button--danger"><?php _e('Delete all WebP files', 'wp-convert-to-webp'); ?></button>
                                 </div>
                             </div>
                         </div>
@@ -148,31 +150,30 @@ class Options
                 </div>
                 <div class="convert-to-webp__comparison">
                     <!-- Image Selector for Comparison UI -->
-                    <form method="post" action="" class="convert-to-webp__form convert-to-webp__form--comparison">
-                        <div class="convert-to-webp__form--table">
-                            <div class="convert-to-webp__form--table__row">
-                                <h2><?php _e('Select an Image for Comparison', 'wp-convert-to-webp'); ?></h2>
-                                <div class="convert-to-webp__form--submit convert-to-webp__form--submit__secondary">
-                                    <button type="button" class="button" id="select-image-button"><?php _e('Select Image', 'wp-convert-to-webp'); ?></button>
+                    <form method="post" action="" id="comparison-form" class="convert-to-webp__form convert-to-webp__form--comparison">
+                        <div class="convert-to-webp__table">
+                            <div class="convert-to-webp__row">
+                                <h2 class="convert-to-webp__subtitle"><?php _e('Select an Image for Comparison', 'wp-convert-to-webp'); ?></h2>
+                                <div class="convert-to-webp__submit convert-to-webp__submit--secondary">
+                                    <button type="button" class="button convert-to-webp__button" id="comparison-button"><?php _e('Select Image', 'wp-convert-to-webp'); ?></button>
                                 </div>
                             </div>
                         </div>
                     </form>
 
                     <!-- Comparison UI -->
-                    <div class="convert-to-webp__compare">
-                        <div class="convert-to-webp__image">
-                            <img id="comparison-original" class="convert-to-webp__image--origin" src="">
-                            <img id="comparison-webp" class="convert-to-webp__image--webp" src="">
+                    <div id="comparison-container" class="convert-to-webp__compare">
+                        <div class="convert-to-webp__images">
+                            <img id="comparison-original" class="convert-to-webp__image convert-to-webp__image--origin" src="">
+                            <img id="comparison-webp" class="convert-to-webp__image convert-to-webp__image--webp" src="">
                         </div>
-                        <input type="range" min="0" max="100" value="50" class="convert-to-webp__range">
+                        <input type="range" min="0" max="100" value="50" id="comparison-range" class="convert-to-webp__range">
                         <div class="convert-to-webp__handler"></div>
-                        <div class="convert-to-webp__compare--details">
-                        <p class="convert-to-webp__compare--size">Original image: <span id="comparison-original-size"></span></p>
-                        <p class="convert-to-webp__compare--size">WebP Image: <span id="comparison-webp-size"></span></p>
+                        <div class="convert-to-webp__sizes">
+                            <p class="convert-to-webp__size">Original size: <strong id="comparison-original-size"></strong></p>
+                            <p class="convert-to-webp__size">WebP size: <strong id="comparison-webp-size"></strong></p>
+                        </div>
                     </div>
-                    </div>
-                    
                 </div>
             </div>
         </div>
@@ -222,53 +223,35 @@ class Options
             wp_die(__('Not allowed', 'wp-convert-to-webp'));
         }
 
-        $quality    = get_option('convert_to_webp_quality', 85);
-        $files      = Tools::get_files();
+        $files          = Tools::get_files();
 
-        if ($files) {
-            foreach ($files as $file) {
-                if ($file->isDir()) {
-                    continue;
-                }
-
-                $extension      = strtolower($file->getExtension());
-
-                // Only process supported image types
-                if (in_array($extension, ['jpg', 'jpeg', 'png', 'gif'])) {
-                    $info       = pathinfo($file->getPathname());
-                    $webp_path  = $info['dirname'] . '/' . $info['filename'] . '.webp';
-                    if (!file_exists($webp_path)) {
-
-                        $image  = null;
-
-                        // Create image resource from file
-                        switch ($extension) {
-                            case 'jpg':
-                            case 'jpeg':
-                                $image = @imagecreatefromjpeg($file->getPathname());
-                                break;
-                            case 'png':
-                                $image = @imagecreatefrompng($file->getPathname());
-                                break;
-                            case 'gif':
-                                $image = @imagecreatefromgif($file->getPathname());
-                                break;
-                        }
-
-                        // Save as WebP if resource is valid
-                        if ($image) {
-                            imagewebp($image, $webp_path, $quality);
-                            imagedestroy($image);
-                        }
-                    }
-                }
-            }
-
-            // Redirect back to the options page with a success flag
-            wp_redirect(add_query_arg('converted', '1', admin_url('admin.php?page=wp-convert-to-webp')));
-
+        if (empty($files)) {
+            wp_redirect(add_query_arg('no_files', '1', admin_url('admin.php?page=wp-convert-to-webp')));
+            
             exit;
         }
+
+        foreach ($files as $file) {
+            if ($file->isDir()) {
+                continue;
+            }
+
+            $filepath   = $file->getPathname();
+
+            $converter  = new Converter();
+            $converter->convert($filepath);
+        }
+
+        // Clear the cache to ensure the new WebP files are recognized
+        wp_cache_flush();
+
+        // Clear also the media library cache
+        wp_update_attachment_metadata(0, []); // Update all attachments
+
+        // Redirect back to the options page with a success flag
+        wp_redirect(add_query_arg('converted', '1', admin_url('admin.php?page=wp-convert-to-webp')));
+        
+        exit;
     }
 
     /**
@@ -284,24 +267,25 @@ class Options
             wp_die(__('Not allowed', 'wp-convert-to-webp'));
         }
 
-        try {
-            $files  = Tools::get_files();
+        $files      = Tools::get_files();
 
-            foreach ($files as $file) {
-                if ($file->isFile() && strtolower($file->getExtension()) === 'webp') {
-                    if (!@unlink($file->getPathname())) {
-                        error_log('[WP Convert to WebP] Failed to delete: ' . $file->getPathname());
-                    }
-
-                    @unlink($file->getPathname());
-                }
-            }
-
-            // Redirect back to the options page with a success flag
-            wp_redirect(add_query_arg('webp_deleted', '1', admin_url('admin.php?page=wp-convert-to-webp')));
+        if (empty($files)) {
+            wp_redirect(add_query_arg('no_files', '1', admin_url('admin.php?page=wp-convert-to-webp')));
             exit;
-        } catch (Throwable $error) {
-            error_log('[WP Convert to WebP] Uninstall error: ' . $error->getMessage());
         }
+
+        $cleaner    = new Cleaner();
+        $cleaner->remove($files);
+
+        // Clear the cache
+        wp_cache_flush();
+
+        // Clear also the media library cache
+        wp_update_attachment_metadata(0, []); // Update all attachments
+
+        // Redirect back to the options page with a success flag
+        wp_redirect(add_query_arg('deleted', '1', admin_url('admin.php?page=wp-convert-to-webp')));
+
+        exit;
     }
 }

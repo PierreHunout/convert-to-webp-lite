4eb3f6dce5392febbaf25c93dd9d952a725ad2fc
PierreHunout <pierre.hunout@gmail.com>
2025-08-14 16:08:08 +0100

    Refactoring the replacer

--- includes/Replacer.php
+++ includes/Replacer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * WebP Replacer Class
  *
@@ -35,8 +36,8 @@ class Replacer
      * @param array $matches The matches from the regular expression.
      * @return string The modified <picture> element or original <img> tag.
      */
-    public function replace($matches)
-    {  
+    public static function replace($matches)
+    {
         $image      = $matches[0];
         $src        = $matches[1];
         $upload_dir = wp_upload_dir();
@@ -48,16 +49,16 @@ class Replacer
 
         // Check if the WebP file exists
         $webp_src   = preg_replace('/\.(jpe?g|png|gif)$/i', '.webp', $src);
-        $is_file    = Tools::check_file($webp_src);
-        
-        if ($is_file) {
-            return sprintf(
-                '<picture><source srcset="%s" type="image/webp">%s</picture>',
-                esc_url($webp_src),
-                $image
-            );
+        $is_file    = Tools::is_file($webp_src);
+
+        if (!$is_file) {
+            return $image;
         }
 
-        return $image;
+        return sprintf(
+            '<picture><source srcset="%s" type="image/webp">%s</picture>',
+            esc_url($webp_src),
+            $image
+        );
     }
 }
